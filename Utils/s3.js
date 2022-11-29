6260dd25c33696f6220743e86ef6d880eb79ce75
hammadaslam10 <muhammadaslambhai642@gmail.com>
2022-11-29 07:54:32 -0800

    done

--- Utils/s3.js
+++ Utils/s3.js
@@ -8,8 +8,8 @@ const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
 require("dotenv").config({ path: "./config/Secrets.env" });
 const bucketName = process.env.AWS_BUCKET_NAME;
 const region = process.env.AWS_BUCKET_REGION;
-const accessKeyId = process.env.AWS_ACCESS_KEYS;
-const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEYS;
+const accessKeyId = process.env.AWS_ACCESS_KEY;
+const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
 
 const s3Client = new S3Client({
   region,
