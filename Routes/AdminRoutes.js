fb593ca019309fa68b3496d0f681f12ad7c85ed3
hasnainmak96 <hasnain.ali@avancerasolution.com>
2022-12-08 18:41:19 +0500

    done12

--- Routes/AdminRoutes.js
+++ Routes/AdminRoutes.js
@@ -16,13 +16,12 @@ const {
   resetPassword,
 } = require("../Controller/AdminController");
 router.route("/getAdmin").get(GetAllAdmin);
-router.route("/register").post(RegisterAdmin);
-router.route("/login").post(LoginAdmin);
-router.route("/register").post(RegisterAdmin);
-router.route("/logout").get(logOut);
+router.route("/adminregister").post(RegisterAdmin);
+router.route("/adminlogin").post(LoginAdmin);
+router.route("/adminlogout").get(logOut);
 router.route("/ChangeStatus/:id").put(AdminApproval);
 router.route("/singleAdmin/:id").get(GetonlyoneAdmin);
-router.route("/password/forgot").post(forgotPassword);
-router.route("/password/reset/:token").put(resetPassword);
+router.route("/adminpassword/forgot").post(forgotPassword);
+router.route("/adminpassword/reset/:token").put(resetPassword);
 
 module.exports = router;
