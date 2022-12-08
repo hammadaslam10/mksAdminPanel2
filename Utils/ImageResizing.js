00425d19b7cb5032f1527e2cc094685b9910dcb8
hasnainmak96 <hasnain.ali@avancerasolution.com>
2022-12-08 18:39:47 +0500

    done12

--- Utils/ImageResizing.js
+++ Utils/ImageResizing.js
@@ -1,12 +1,11 @@
 const sharp = require("sharp");
 exports.resizeImageBuffer = async (imageBuffer, width, height) => {
   const sharpImg = await sharp(imageBuffer);
-  return sharpImg
-    .resize({
-      width: width,
-      height: height,
-      fit: "contain",
-      background: { r: 255, g: 255, b: 255, alpha: 1 },
-    })
-    .toBuffer();
+  return sharpImg.resize().toBuffer();
+  // {
+  //   width: width,
+  //   height: height,
+  //   fit: "contain",
+  //   background: { r: 255, g: 255, b: 255, alpha: 1 },
+  // }
 };
