67b05d415070dd30919c20b644886712d22c168a
hasnainmak96 <hasnain.ali@avancerasolution.com>
2022-12-15 20:14:12 +0500

    done23

--- Models/AdvertismentModel.js
+++ Models/AdvertismentModel.js
@@ -17,28 +17,44 @@ module.exports = (sequelize, DataTypes) => {
         allowNull: false,
         validate: {
           notNull: { msg: "Please Add Image Of Advertisment" },
-          notEmpty: { msg: "Without Image Advertisment Will not be get submit" },
+          notEmpty: {
+            msg: "Without Image Advertisment Will not be get submit",
+          },
         },
       },
       DescriptionEn: {
         type: DataTypes.STRING,
         allowNull: false,
         validate: {
-          is: /^[A-Za-z\s]*$/,
-          is: {
-            msg: "Please enter the Description in English",
+          EnglishLanguageVerification() {
+            if (this.DescriptionEn.trim() == "") {
+              throw new Error("Description English is Empty");
+            }
+            if (
+              /^[a-zA-Z0-9$@$!%*?&#^-_.+]+$/.test(this.DescriptionEn) ||
+              /^[a-zA-Z0-9$@$!%*?&#^-_. +]/.test(this.DescriptionEn)
+            ) {
+            } else {
+              throw new Error("Description English Validation Failed");
+            }
           },
-          notNull: { msg: "Description in English is Null" },
-          notEmpty: { msg: "Description in English is Empty" },
         },
       },
       DescriptionAr: {
         type: DataTypes.STRING,
         allowNull: false,
         validate: {
-          is: /^[\u0621-\u064A\u0660-\u0669 ]+$/,
-          is: {
-            msg: "Description Must Be In Arabic",
+          ArabicLanguageVerification() {
+            if (this.DescriptionAr.trim() == "") {
+              throw new Error("Description Arabic is Empty");
+            }
+            if (
+              /^[\u0621-\u064A\u0660-\u06690-9 ]+$/.test(this.DescriptionAr) ||
+              /^[\u0621-\u064A\u0660-\u06690-9]+$/.test(this.DescriptionAr)
+            ) {
+            } else {
+              throw new Error("Description Arabic Validation Failed");
+            }
           },
         },
       },
@@ -46,12 +62,18 @@ module.exports = (sequelize, DataTypes) => {
         type: DataTypes.STRING,
         allowNull: false,
         validate: {
-          is: /^[A-Za-z\s]*$/,
-          is: {
-            msg: "Title English Must Be In English",
+          EnglishLanguageVerification() {
+            if (this.TitleEn.trim() == "") {
+              throw new Error("Description English is Empty");
+            }
+            if (
+              /^[a-zA-Z0-9$@$!%*?&#^-_.+]+$/.test(this.TitleEn) ||
+              /^[a-zA-Z0-9$@$!%*?&#^-_. +]/.test(this.TitleEn)
+            ) {
+            } else {
+              throw new Error("Description English Validation Failed");
+            }
           },
-          notNull: { msg: "Ad will have Title" },
-          notEmpty: { msg: "Title   will not be empty" },
         },
       },
       TitleAr: {
