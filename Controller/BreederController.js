0ea5bdb887453ba662527c2861c54a830acca534
hasnainmak96 <hasnain.ali@avancerasolution.com>
2022-12-26 13:48:44 +0500

    done

--- Controller/BreederController.js
+++ Controller/BreederController.js
@@ -60,8 +60,7 @@ exports.CreateBreeder = Trackerror(async (req, res, next) => {
     TitleAr,
     shortCode,
   } = req.body;
-  const file = req.files.image;
-  if (!file) {
+  if (req.files === null) {
     try {
       const data = await BreederModel.create({
         image: `https://${
@@ -96,40 +95,42 @@ exports.CreateBreeder = Trackerror(async (req, res, next) => {
         });
       }
     }
-  }
-  const Image = generateFileName();
-  const fileBuffer = await resizeImageBuffer(req.files.image.data, 214, 212);
-  await uploadFile(fileBuffer, `${Breeder}/${Image}`, file.mimetype);
-  try {
-    const data = await BreederModel.create({
-      image: `https://${process.env.AWS_BUCKET_NAME}.s3.amazonaws.com/${Breeder}/${Image}`,
-      DescriptionEn: DescriptionEn,
-      DescriptionAr: DescriptionAr,
-      shortCode: shortCode,
-      TitleEn: TitleEn,
-      TitleAr: TitleAr,
-      NameEn: NameEn,
-      NameAr: NameAr,
-    });
-    res.status(201).json({
-      success: true,
-      data,
-    });
-  } catch (error) {
-    if (error.name === "SequelizeUniqueConstraintError") {
-      res.status(403);
-      res.send({
-        status: "error",
-        message:
-          "This Short Code already exists, Please enter a different one.",
+  } else {
+    const file = req.files.image;
+    const Image = generateFileName();
+    const fileBuffer = await resizeImageBuffer(req.files.image.data, 214, 212);
+    await uploadFile(fileBuffer, `${Breeder}/${Image}`, file.mimetype);
+    try {
+      const data = await BreederModel.create({
+        image: `https://${process.env.AWS_BUCKET_NAME}.s3.amazonaws.com/${Breeder}/${Image}`,
+        DescriptionEn: DescriptionEn,
+        DescriptionAr: DescriptionAr,
+        shortCode: shortCode,
+        TitleEn: TitleEn,
+        TitleAr: TitleAr,
+        NameEn: NameEn,
+        NameAr: NameAr,
       });
-    } else {
-      res.status(500).json({
-        success: false,
-        message: error.errors.map((singleerr) => {
-          return singleerr.message;
-        }),
+      res.status(201).json({
+        success: true,
+        data,
       });
+    } catch (error) {
+      if (error.name === "SequelizeUniqueConstraintError") {
+        res.status(403);
+        res.send({
+          status: "error",
+          message:
+            "This Short Code already exists, Please enter a different one.",
+        });
+      } else {
+        res.status(500).json({
+          success: false,
+          message: error.errors.map((singleerr) => {
+            return singleerr.message;
+          }),
+        });
+      }
     }
   }
 });
