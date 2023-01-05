27375eefd25b6c640b7d7a4c7535ac1ff708d15b
hasnainmak96 <hasnain.ali@avancerasolution.com>
2023-01-05 20:08:48 +0500

    done

--- Controller/BreederController.js
+++ Controller/BreederController.js
@@ -97,9 +97,7 @@ exports.BreederMassUpload = Trackerror(async (req, res, next) => {
       // } else {
       res.status(500).json({
         success: false,
-        message: error.errors.map((singleerr) => {
-          return singleerr.message;
-        }),
+        message: error.errors,
       });
       // }
     }
