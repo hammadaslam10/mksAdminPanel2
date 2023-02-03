33e0379c0067cfe317c71789917cf421ec11935a
hasnainmak96 <hasnain.ali@avancerasolution.com>
2023-02-03 19:29:14 +0500

    done

--- Controller/CurrencyController.js
+++ Controller/CurrencyController.js
@@ -236,6 +236,68 @@ exports.DeleteCurrency = Trackerror(async (req, res, next) => {
     message: "data Delete Successfully",
   });
 });
+exports.CurrencyMassUpload = Trackerror(async (req, res, next) => {
+  if (!req.files || !req.files.file) {
+    res.status(404).json({ message: "File not found" });
+  } else if (req.files.file.mimetype === "application/json") {
+    let de = JSON.parse(req.files.file.data.toString("utf8"));
+    // console.log(de);
+    let original = [];
+    // let ShortCodeValidation = [];
+    // await de.map((data) => {
+    //   ShortCodeValidation.push(data.shortCode);
+    // });
+    // const Duplicates = await CurrencyModel.findAll({
+    //   where: {
+    //     shortCode: ShortCodeValidation,
+    //   },
+    // });
+    // if (Duplicates.length >= 1) {
+    //   res.status(215).json({
+    //     success: false,
+    //     Notify: "Duplication Error",
+    //     message: {
+    //       ErrorName: "Duplication Error",
+    //       list: Duplicates.map((singledup) => {
+    //         return {
+    //           id: singledup.BackupId,
+    //           shortCode: singledup.shortCode,
+    //           NameEn: singledup.NameEn,
+    //           NameAr: singledup.NameAr,
+    //         };
+    //       }),
+    //     },
+    //   });
+    //   res.end();
+    // } else {
+    await de.map((data) => {
+      original.push({
+        // shortCode: data.shortCode,
+        NameEn: data.NameEn,
+        NameAr: data.NameAr,
+        DescriptionAr: data.DescriptionAr || data.NameAr,
+        TitleEn: data.TitleEn || data.NameEn,
+        DescriptionAr: data.DescriptionAr || data.NameAr,
+        DescriptionEn: data.DescriptionEn || data.NameEn,
+        BackupId: data.id,
+      });
+    });
+
+    try {
+      const data = await CurrencyModel.bulkCreate(original);
+      // /{ validate: true }
+      res.status(201).json({ success: true, data });
+      // }
+    } catch (error) {
+      res.status(500).json({
+        success: false,
+        message: error,
+      });
+    }
+  } else {
+    res.status(409).json({ message: "file format is not valid" });
+  }
+});
 exports.SoftDeleteCurrency = Trackerror(async (req, res, next) => {
   const data = await CurrencyModel.findOne({
     where: { _id: req.params.id },
